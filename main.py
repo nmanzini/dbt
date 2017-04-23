780d17609eceeb7a03c12bb965f11f75d30f8ee4
Andrew Pelletier <pelletierra@gmail.com>
2017-04-23 01:20:11 -0700

    cleaning up tabs/spaces thanks to pycharm, also removed excess scheduler import for now

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 
 import get_urls
 import image_downloader
-#import schedule
 import time
 import set_image
 
@@ -19,20 +18,17 @@ PAGES = 3
 def main():
     """
     Gets reddit pic URLs from given subreddit already filtered
-	Downloads the pictures
-	Sets Desktop Background
-	"""
+    Downloads the pictures
+    Sets Desktop Background
+    """
     pic_urls = get_urls.reddit_pics(SUBREDDITS, PAGES)
     image_downloader.check_folder(FOLDER_PATH)
     image_downloader.download_pics(pic_urls, FOLDER_PATH)
 
-    #list of file in the pictures folder
+    # list of file in the pictures folder
     images = [file for file in os.listdir(FOLDER_PATH)]
 
-    #code with schedule library
-    #schedule.every(10).seconds.do(set_back, images)
     while len(images) > 0:
-        #schedule.run_pending()
         set_back(images)
         time.sleep(5)
 
@@ -41,7 +37,7 @@ def set_back(images_list):
     """
     function that pops an image from the list ad sets it as background
     :param images_list: names of images
-    :type images_list: string
+    :type images_list: list of strings
     :return: changes background
     :rtype: none
     """
