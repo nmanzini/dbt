aec8a3b0d9ebe9002940248e0c0698af26728987
pelletierra <pelletierra@gmail.com>
2017-04-22 17:35:53 -0700

    cleared up merge conflict, all good now

--- main.py
+++ main.py
@@ -7,33 +7,34 @@ import time
 import set_image
 
 # subreddit to pull pictures from
-SUBREDDIT = "/r/wallpapers"
+SUBREDDITS = ["wallpapers", "EarthPorn", "WQHD_Wallpaper"]
 
 # pictures folder path in current working directory (cwd)
 FOLDER_PATH = os.getcwd() + "\\pictures\\"
 
+# number of pages to go through (up to 25 pictures per page)
+PAGES = 3
 
-def main(subreddit):
+
+def main():
     """
     Gets reddit pic URLs from given subreddit already filtered
 	Downloads the pictures
 	Sets Desktop Background
 	"""
-    pic_urls = get_urls.reddit_pics(subreddit)
+    pic_urls = get_urls.reddit_pics(SUBREDDITS, PAGES)
     image_downloader.check_folder(FOLDER_PATH)
     image_downloader.download_pics(pic_urls, FOLDER_PATH)
-
 
     #list of file in the pictures folder
     images = [file for file in os.listdir(FOLDER_PATH)]
 
-
-    #code with scheulde library
+    #code with schedule library
     #schedule.every(10).seconds.do(set_back, images)
     while len(images) > 0:
         #schedule.run_pending()
         set_back(images)
-        time.sleep(10)
+        time.sleep(5)
 
 
 def set_back(images_list):
@@ -48,5 +49,5 @@ def set_back(images_list):
     set_image.set_image(FOLDER_PATH+image)
 
 
-main(SUBREDDIT)
+main()
 
