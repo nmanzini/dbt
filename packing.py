2ef7ccb06c13fd121c20202432c3a451b3037e8d
nmanzini <manzini.n@gmail.com>
2017-05-25 18:51:18 +0200

    updated with instruction on how to pack

--- packing.py
+++ packing.py
@@ -1,9 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+'''
+To pack the program go to cmd in the folder and type:
+python packing.py build
+
+to pack it into an installer, open cmd in the folder and type:
+python packing.py bdist_msi
+'''
+
+
 application_title = "Desktop background Tool" #what you want to application to be called
 main_python_file = "gui.py" #the name of the python file you use to run the program
-
-import sys
-
-from cx_Freeze import setup, Executable
 
 base = None
 if sys.platform == "win32":
